5fa0f363229d52cbc21dfd44882850c8b6506959
henriquepe <henrique.p.espindola@gmail.com>
2020-08-14 15:40:42 -0300

    for deploy

---
+++ build/precache-manifest.9e3efff6c4cd262517158013b6b8335d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4e5c345ece46f6278bd721b0c78ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857ae9c0798daa16a687",
+    "url": "/static/js/2.bf3ede55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf3ede55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77ef33808ef19ea5d8e",
+    "url": "/static/js/main.eb68b0a4.chunk.js"
+  },
+  {
+    "revision": "9d536a9f040438b52e43",
+    "url": "/static/js/runtime-main.06335b0d.js"
+  },
+  {
+    "revision": "2abb2ff5a4435bf45b8c83fd887ffe0b",
+    "url": "/static/media/background_1280.2abb2ff5.png"
+  }
+]);
